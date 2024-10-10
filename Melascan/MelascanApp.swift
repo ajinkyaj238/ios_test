29342b4567c5df2d2387ee9f2e055e507ca21d1d
ajinkyaj238 <ajinkyaj238@gmail.com>
2024-10-10 16:34:17 -0400

    Initial Commit

---
+++ Melascan/MelascanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelascanApp.swift
+//  Melascan
+//
+//  Created by Ajinkya Joshi on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MelascanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MelascanTests/MelascanTests.swift
@@ -0,0 +1,17 @@
+//
+//  MelascanTests.swift
+//  MelascanTests
+//
+//  Created by Ajinkya Joshi on 10/10/24.
+//
+
+import Testing
+@testable import Melascan
+
+struct MelascanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
